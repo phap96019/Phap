c3e389c176ed03b7a4d9426b34bece7b40bae614
Yen Mai <yenmai478@gmail.com>
2019-10-09 21:14:22 +0700

    Yen commit

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ const app = express();
 
 const PORT = 3001;
 
-app.get("/", (req,res)=> {
-    res.json({msg: "Hello from PDC"});
-})
+app.get("/", (req, res) => {
+  res.json({ msg: "Hello from PDC" });
+});
 
-
-app.listen(PORT, ()=> {
-    console.log("Server is running on 3001");
-})
+app.listen(PORT, () => {
+  console.log("Server is running on " + PORT);
+});
